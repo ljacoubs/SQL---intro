e1ccac977f65a23c76bea131fc68c6de9b817da5
Lou Jacoubs <lou.jacoubs@kellogg.northwestern.edu>
2023-04-12 16:37:33 +0000

    finished exercises from last week class

--- school-2-drop-and-create.sql
+++ school-2-drop-and-create.sql
@@ -1,3 +1,10 @@
 -- Drop the students table
 
 -- Recreate it
+drop table students;
+CREATE TABLE students (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT,
+  email TEXT
+);
---
+++ school-5-insert.sql
@@ -0,0 +1,13 @@
+INSERT INTO students (
+  first_name,
+  last_name,
+  email
+)
+VALUES (
+  "Jane",
+  "Doe",
+  "jane@example.com"
+);
+
+select *
+from students
